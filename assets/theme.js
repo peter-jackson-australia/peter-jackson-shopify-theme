5412c0faec443493c2dba9ea187dcbae33dc4acf
Ash Madden <ashleymadden55@gmail.com>
2025-03-10 18:06:58 +1100

    reduced unused assets

--- assets/theme.js
+++ assets/theme.js
@@ -1,10 +1,9 @@
 // Define a convenience method and use it like
 // ready(() => {...});
 var ready = (callback) => {
-  if (document.readyState != "loading") callback();
-  else document.addEventListener("DOMContentLoaded", callback);
+  if (document.readyState != 'loading') callback();
+  else document.addEventListener('DOMContentLoaded', callback);
 };
-
 
 /*
  * HISTORY STATE
@@ -12,8 +11,7 @@ var ready = (callback) => {
  */
 function historyState() {
   return window.history && window.history.replaceState;
-};
-
+}
 
 /*
  * GET URL PARAMETER
@@ -25,7 +23,7 @@ function getParam(name) {
     return params.get(name);
   } else {
     // Polyfill for IE11
-    var params = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    var params = new RegExp('[?&]' + name + '=([^&#]*)').exec(window.location.href);
     if (params == null) {
       return null;
     } else {
@@ -34,22 +32,23 @@ function getParam(name) {
   }
 }
 
-
 /*
  * FORMAT MONEY
  * Formats the passed value as money with currency symbol.
  * Referenced from https://gist.github.com/stewartknapman/8d8733ea58d2314c373e94114472d44c
  */
 var Shopify = Shopify || {};
-Shopify.money_format = "${{amount}}";
-Shopify.formatMoney = function(cents, format) {
-  if (typeof cents == 'string') { cents = cents.replace('.', ''); }
+Shopify.money_format = '${{amount}}';
+Shopify.formatMoney = function (cents, format) {
+  if (typeof cents == 'string') {
+    cents = cents.replace('.', '');
+  }
   var value = '';
   var placeholderRegex = /\{\{\s*(\w+)\s*\}\}/;
-  var formatString = (format || this.money_format);
+  var formatString = format || this.money_format;
 
   function defaultOption(opt, def) {
-    return (typeof opt == 'undefined' ? def : opt);
+    return typeof opt == 'undefined' ? def : opt;
   }
 
   function formatWithDelimiters(number, precision, thousands, decimal) {
@@ -57,13 +56,15 @@ Shopify.formatMoney = function(cents, format) {
     thousands = defaultOption(thousands, ',');
     decimal = defaultOption(decimal, '.');
 
-    if (isNaN(number) || number == null) { return 0; }
+    if (isNaN(number) || number == null) {
+      return 0;
+    }
 
     number = (number / 100.0).toFixed(precision);
 
     var parts = number.split('.'),
       dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + thousands),
-      cents = parts[1] ? (decimal + parts[1]) : '';
+      cents = parts[1] ? decimal + parts[1] : '';
 
     return dollars + cents;
   }
@@ -86,44 +87,43 @@ Shopify.formatMoney = function(cents, format) {
   return formatString.replace(placeholderRegex, value);
 };
 
-
 /*
  * OPTION SELECTOR
  * Works the variant option dropdowns used on the
  * product page template.
  */
-ready(function() {
+ready(function () {
   var variant;
   var options = [];
   options[1] = null;
   options[2] = null;
   options[3] = null;
 
-  var variantOptions = document.querySelectorAll(".js--variant-option");
-  variantOptions.forEach(function(el) {
-    el.addEventListener("change", function(event) {
+  var variantOptions = document.querySelectorAll('.js--variant-option');
+  variantOptions.forEach(function (el) {
+    el.addEventListener('change', function (event) {
       // Disable non-existent variant options.
       checkVariants();
 
       // Set the chosen options.
-      variantOptions.forEach(function(opt) {
+      variantOptions.forEach(function (opt) {
         if (opt.tagName.toLowerCase() == 'input' && opt.checked == true) {
-          var optionName = opt.getAttribute("name");
-          var optionPos = parseInt(optionName.replace("option", ""));
+          var optionName = opt.getAttribute('name');
+          var optionPos = parseInt(optionName.replace('option', ''));
           var optionValue = opt.value;
           options[optionPos] = optionValue;
         }
       });
 
       // Loop through the variants and get the selected one.
-      variants.filter(function(v) {
+      variants.filter(function (v) {
         if (v.option1 == options[1] && v.option2 == options[2] && v.option3 == options[3]) {
           variant = v;
 
           // Update variant id and prices
-          document.querySelector("input#js--variant-id").value = v.id;
+          document.querySelector('input#js--variant-id').value = v.id;
           // The following is used for the "purchase together" feature.
-          document.querySelectorAll('input[type="checkbox"].js--variant-id').forEach(function(el) {
+          document.querySelectorAll('input[type="checkbox"].js--variant-id').forEach(function (el) {
             el.value = v.id;
             el.setAttribute('data-price', v.price);
             if (v.available == true) {
@@ -134,46 +134,46 @@ ready(function() {
               el.disabled = true;
             }
           });
-          document.querySelectorAll(".js--variant-price").forEach(function(el) {
+          document.querySelectorAll('.js--variant-price').forEach(function (el) {
             el.innerHTML = Shopify.formatMoney(v.price);
           });
 
           if (v.compare_at_price > v.price) {
-            document.querySelectorAll(".js--variant-compareatprice").forEach(function(el) {
+            document.querySelectorAll('.js--variant-compareatprice').forEach(function (el) {
               el.innerText = Shopify.formatMoney(v.compare_at_price);
             });
           } else {
-            document.querySelectorAll(".js--variant-compareatprice").forEach(function(el) {
-              el.innerText = "";
+            document.querySelectorAll('.js--variant-compareatprice').forEach(function (el) {
+              el.innerText = '';
             });
           }
 
           // Update SKU
-          document.querySelectorAll('.js--variant-sku').forEach(function(el) {
+          document.querySelectorAll('.js--variant-sku').forEach(function (el) {
             el.innerText = variant.sku;
           });
 
           // Disable the buy button if product is unavailable
           if (v.available === false) {
-            document.querySelector("#js--addtocart").disabled = true;
-            document.querySelector("#js--addtocart").innerText = "Unavailable";
+            document.querySelector('#js--addtocart').disabled = true;
+            document.querySelector('#js--addtocart').innerText = 'Unavailable';
           } else {
-            document.querySelector("#js--addtocart").disabled = false;
-            document.querySelector("#js--addtocart").innerText = "Add to cart";
+            document.querySelector('#js--addtocart').disabled = false;
+            document.querySelector('#js--addtocart').innerText = 'Add to cart';
           }
 
           // Update image
-          var imageId = (v.featured_image != null) ? v.featured_image.id : default_image;
+          var imageId = v.featured_image != null ? v.featured_image.id : default_image;
           var imageIndex = document
             .querySelector('.product-image-item[data-imageid="' + imageId + '"]')
-            .getAttribute("data-index");
-          var sliderContainer = document.querySelector(".product-images-component");
+            .getAttribute('data-index');
+          var sliderContainer = document.querySelector('.product-images-component');
           goToImage(imageIndex, sliderContainer);
 
           // Append the variant ID as a url parameter
           if (v != undefined) {
             if (historyState()) {
-              window.history.replaceState({}, document.title, "?variant=" + v.id);
+              window.history.replaceState({}, document.title, '?variant=' + v.id);
             }
           }
         }
@@ -192,7 +192,7 @@ function checkVariants() {
   let $this = event.target;
   if ($this !== undefined) {
     let availableVariants = new Set();
-    variants.filter(function(variant, k) {
+    variants.filter(function (variant, k) {
       if (variant[$this.name] == $this.value) {
         availableVariants.add(variant);
       }
@@ -201,7 +201,7 @@ function checkVariants() {
     // Loop through the array above to create an array containing available
     // option values.
     let optionGroups = {};
-    availableVariants.forEach(function(variant) {
+    availableVariants.forEach(function (variant) {
       let options = Object.entries(variant);
       for (const [key, value] of options) {
         if (value != null) {
@@ -217,7 +217,7 @@ function checkVariants() {
 
     // Then, loop through each input and check if its value is in the optionGroups
     // array created above, ignoring the clicked option group.
-    document.querySelectorAll(".js--variant-option").forEach(function(input) {
+    document.querySelectorAll('.js--variant-option').forEach(function (input) {
       if (input.name != $this.name) {
         if (optionGroups[input.name].includes(input.value) == false) {
           input.disabled = true;
@@ -230,11 +230,11 @@ function checkVariants() {
   }
 
   // Check a valid option is selected for each group.
-  document.querySelectorAll(".js--variant-options").forEach(function(group) {
+  document.querySelectorAll('.js--variant-options').forEach(function (group) {
     let firstAvailable = null;
     let checkedOptions = group.querySelectorAll('.js--variant-option:checked').length;
     if (checkedOptions == 0) {
-      firstAvailable = group.querySelectorAll(".js--variant-option:not(:disabled)")[0];
+      firstAvailable = group.querySelectorAll('.js--variant-option:not(:disabled)')[0];
     }
     if (firstAvailable != null) {
       firstAvailable.checked = true;
