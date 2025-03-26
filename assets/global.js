bad32ff322f5e3faaee2586e15a2cb8b57a7eaaf
ash-madden <ash@peterjacksons.com>
2025-03-27 10:19:38 +1100

    Added Blaze slider init script

---
+++ assets/global.js
@@ -0,0 +1,9 @@
+// Quicklink provides faster subsequent page-loads by prefetching in-viewport links during idle time
+window.addEventListener("load", () => {
+  quicklink.listen();
+});
+
+// Initialise Blaze Sliders
+document.querySelectorAll(".blaze-slider").forEach((el) => {
+  new BlazeSlider(el, options);
+});
