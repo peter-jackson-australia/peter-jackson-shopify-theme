3a5f5e2e520453c9eb14052ce86d0a987d59e4f7
ash-madden <ash@peterjacksons.com>
2025-05-06 17:48:20 +1000

    Add to cart fix

--- assets/global.js
+++ assets/global.js
@@ -182,7 +182,6 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll('form[action="/cart/add"]').forEach((form) => {
     form.addEventListener("submit", async (e) => {
       e.preventDefault();
-      openCartDrawer();
       const addToCartButton = form.querySelector("#js--addtocart");
       if (addToCartButton && !addToCartButton.disabled) {
         const originalText = addToCartButton.innerHTML;
@@ -196,6 +195,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
           const cart = await fetchAndUpdateCart();
           await updateCartDrawer();
+          openCartDrawer();
         } catch (e) {
           console.error("Error adding to cart:", e);
         } finally {
