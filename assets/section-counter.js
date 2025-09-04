f9be432eb2dc73cbadd3f60a71a466936b4c41c1
Jed Saxon <jed@peterjacksons.com>
2025-09-04 12:09:43 +1000

    fix up broken css and js after refactor

--- assets/section-counter.js
+++ assets/section-counter.js
@@ -1,37 +1,41 @@
-const sectionId = document.currentScript.dataset.sectionId
-const counters = document.querySelectorAll(`#counter-${sectionId} .counter__counters-value`)
+{ // In custom scope to avoid conflicting local variables
+  const sectionId = document.currentScript.dataset.sectionId;
+  const counters = document.querySelectorAll(`#counter-${sectionId} .counter__counters-value`);
+  console.log(counters);
 
-for (let i = 0; i < counters.length; i++) {
-  const counter = counters[i]
+  for (let i = 0; i < counters.length; i++) {
+    const counter = counters[i];
 
-  const initialValue = parseFloat(counter.dataset.initial)
-  const duration = parseFloat(counter.dataset.duration)
+    const initialValue = parseFloat(counter.dataset.initial);
+    const finalValue = parseFloat(counter.dataset.final);
+    const duration = parseFloat(counter.dataset.duration);
 
-  const finalDecimalValue = decimalStrFromNumber(finalValue)
-  const finalDecimalLength = finalDecimalValue ? finalDecimalValue.length : 1;
+    const finalDecimalValue = decimalStrFromNumber(finalValue);
+    const finalDecimalLength = finalDecimalValue ? finalDecimalValue.length : 0;
 
-  counter.textContent = finalValue.toFixed(0)
-  gsap.from(`#${counter.id}`, {
-    textContent: initialValue,
-    duration: duration,
-    ease: Power1.power1out,
-    snap: { textContent: 1 },
-    stagger: {
-      each: 0,
-      start: 'start',
-      onUpdate: function () {
-        const target = this.targets()[0]
-        const num = parseFloat(target.textContent).toFixed(0);
+    counter.textContent = finalValue.toFixed(0);
+    gsap.from(`#${counter.id}`, {
+      textContent: initialValue,
+      duration: duration,
+      ease: Power1.power1out,
+      snap: { textContent: 1 },
+      stagger: {
+        each: 0,
+        start: "start",
+        onUpdate: function () {
+          const target = this.targets()[0];
+          const num = parseFloat(target.textContent).toFixed(0);
 
-        let innerHTML = num.toString()
-        if (finalDecimalLength) innerHTML += `.${finalDecimalLength}`
-        target.textContent = innerHTML
+          let innerHTML = num.toString();
+          if (finalDecimalLength) innerHTML += `.${finalDecimalLength}`;
+          target.textContent = innerHTML;
+        },
       },
-    }
-  })
-}
+    });
+  }
 
-function decimalStrFromNumber(num) {
-  const fraction = num.toString().split('.')
-  fraction.length >= 2 ? fraction[1] : undefined
+  function decimalStrFromNumber(num) {
+    const fraction = num.toString().split(".");
+    fraction.length >= 2 ? fraction[1] : undefined;
+  }
 }
