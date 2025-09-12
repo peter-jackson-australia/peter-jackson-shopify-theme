0431518ddfa5e4dbbdfa76ddec73a9757a3699ed
Jed Saxon <jed@peterjacksons.com>
2025-09-12 12:06:35 +1000

    move javascript from shop-the-look-global.liquid into custom javascript file

---
+++ assets/section-shop-the-look-global.js
@@ -0,0 +1,16 @@
+(() => {
+  const sectionId = document.currentScript.getAttribute("data-section-id")
+  const splideElement = document.querySelector(`#section-${sectionId} .shop-the-look-global__slider`);
+
+  if (splideElement) {
+    new Splide(splideElement, {
+      perPage: 1,
+      type: 'loop',
+      gap: 'var(--space-m)',
+      arrows: true,
+      pagination: false,
+      drag: true,
+      snap: true,
+    }).mount();
+  }
+})();
