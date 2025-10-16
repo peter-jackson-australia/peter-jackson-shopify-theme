829100b39791612a4be99a522492482deddf3014
Jed Saxon <jed@peterjacksons.com>
2025-10-16 14:57:06 +1100

    improve docs

--- assets/section-shop-the-look.js
+++ assets/section-shop-the-look.js
@@ -36,65 +36,10 @@
     return window.innerWidth < 1300;
   }
 
-  /** Generates a splide instance for products in the shop-the-look section, outside the modal */
-  const initialiseShopTheLoopSlider = (elem) => {
-    new Splide(elem, {
-      perPage: 1,
-      type: 'loop',
-      gap: 'var(--space-m)',
-      direction: 'ttb',
-      pagination: false,
-      height: "100%",
-      drag: true,
-      snap: true,
-    }).mount();
-  }
-
-  /** Generates a splide instance for all products inside the modal */
-  const initialiseProductsInModalSlider = (startIdx = 0) => {
-    return new Splide(modalSlider, {
-      type: "fade",
-      arrows: false,
-      start: startIdx,
-      drag: false,
-      pagination: false,
-    }).mount();
-  }
-
-  /** Generates a splide instance for all images for a product. If a scrollbar exists, it will initialise it too */
-  const initialiseProductImagesSplide = (isMobile) => (element) => {
-    const direction = isMobile ? "ltr" : "ttb"
-
-    const newSplide = new Splide(element, {
-      type: "loop",
-      direction: direction,
-      pagination: false,
-      arrows: isMobile,
-      height: "100%",
-      drag: true,
-      snap: true,
-    })
-
-    const progressBar = element.querySelector(".shop-the-look__product-images-progress > .shop-the-look__product-images-progress-bar")
-    updateProductImageSliderHeight(newSplide, progressBar)
-
-    return newSplide
-  }
-
-  const updateProductImageSliderHeight = (splideInstance, sliderElement) => {
-    splideInstance.on("mounted move", function () {
-      const end = splideInstance.Components.Controller.getEnd() + 1;
-      const rate = Math.min((splideInstance.index + 1) / end, 1);
-
-      if (isMobile()) {
-        sliderElement.style.width = String(100 * rate) + "%";
-        sliderElement.style.height = "100%";
-      } else {
-        sliderElement.style.height = String(100 * rate) + "%";
-        sliderElement.style.width = "100%";
-      }
-    });
-  }
+  /**----------------------------------------------------
+   *                 GENERIC UTILS
+   * Should be refactored into other files at some point
+   *----------------------------------------------------*/
 
   /**
    * Freezes the document's height for modals, by setting the main body element's position to `"fixed"`. Will also set
@@ -146,6 +91,120 @@
       left: 0,
       behavior: "instant"
     })
+  }
+
+  /**
+   * If `tabElement` contains the property `data-tab`, and is equal to `selectedTabId`, it will add `selectedClassName`
+   * to it's class list. If it is not equal,
+   * @param selectedTabId {string}
+   * @param selectedClassName {string}
+   * @param tabElement {Element}
+   */
+  const swapTabClassNamesOnSelected = (selectedTabId, selectedClassName) => (tabElement) => {
+    const tabButtonId = tabElement.getAttribute("data-tab")
+    if (tabButtonId == selectedTabId) {
+      if (!tabElement.classList.contains(selectedClassName)) {
+        tabElement.classList.add(selectedClassName)
+      }
+    } else if (tabElement.classList.contains(selectedClassName))  {
+      tabElement.classList.remove(selectedClassName)
+    }
+  }
+
+  /**
+   * Using the `data-tab` attribute of `clickEvent.target`, this will set any button/content with the same `data-tab`
+   * as the target to `tab-collection__tab/content--active`.
+   * @param tabContentContainers {NodeListOf<Element>} All tab content elements of the tab container
+   * @param tabButtons {NodeListOf<Element>} All tab buttons of the tab container
+   * @returns {(function(Event): void)|*}
+   */
+  const onTabButtonClick = (tabButtons, tabContentContainers) => (clickEvent) => {
+    const tabElem = clickEvent.target
+    const tabId = tabElem.getAttribute("data-tab")
+    if (!tabId || tabId == "") return
+
+    tabButtons.forEach(swapTabClassNamesOnSelected(tabId, "tab-collection__tab--active"))
+    tabContentContainers.forEach(swapTabClassNamesOnSelected(tabId, "tab-collection__content--active"))
+  }
+
+  /**
+   * Initialises all tab buttons and content together.
+   * @param rootTabCollection {Element} The root tab collection element, which contains one of each
+   *                                    `.tab-collection__tab` and `tab-collection__content` classes
+   */
+  const initTabCollection = (rootTabCollection) => {
+    const tabButtons = rootTabCollection.querySelectorAll(".tab-collection__tab")
+    const tabCollections = rootTabCollection.querySelectorAll(".tab-collection__content")
+
+    tabButtons.forEach(tabButton => {
+      tabButton.addEventListener("click", onTabButtonClick(tabButtons, tabCollections) )
+    })
+  }
+
+  /**---------------------------------------------------------
+   *                SHOP THE LOOK FUNCTIONS
+   * Used primarily by this code. Do not move from this file
+   *---------------------------------------------------------*/
+
+  /** Generates a splide instance for products in the shop-the-look section, outside the modal */
+  const initialiseShopTheLoopSlider = (elem) => {
+    new Splide(elem, {
+      perPage: 1,
+      type: 'loop',
+      gap: 'var(--space-m)',
+      direction: 'ttb',
+      pagination: false,
+      height: "100%",
+      drag: true,
+      snap: true,
+    }).mount();
+  }
+
+  /** Generates a splide instance for all products inside the modal */
+  const initialiseProductsInModalSlider = (startIdx = 0) => {
+    return new Splide(modalSlider, {
+      type: "fade",
+      arrows: false,
+      start: startIdx,
+      drag: false,
+      pagination: false,
+    }).mount();
+  }
+
+  /** Generates a splide instance for all images for a product. If a scrollbar exists, it will initialise it too */
+  const initialiseProductImagesSplide = (isMobile) => (element) => {
+    const direction = isMobile ? "ltr" : "ttb"
+
+    const newSplide = new Splide(element, {
+      type: "loop",
+      direction: direction,
+      pagination: false,
+      arrows: isMobile,
+      height: "100%",
+      drag: true,
+      snap: true,
+    })
+
+    const progressBar = element.querySelector(".shop-the-look__product-images-progress > .shop-the-look__product-images-progress-bar")
+    updateProductImageSliderHeight(newSplide, progressBar)
+
+    return newSplide
+  }
+
+  /** Manages the width/height of the progress bar for image slides in the modal */
+  const updateProductImageSliderHeight = (splideInstance, sliderElement) => {
+    splideInstance.on("mounted move", function () {
+      const end = splideInstance.Components.Controller.getEnd() + 1;
+      const rate = Math.min((splideInstance.index + 1) / end, 1);
+
+      if (isMobile()) {
+        sliderElement.style.width = String(100 * rate) + "%";
+        sliderElement.style.height = "100%";
+      } else {
+        sliderElement.style.height = String(100 * rate) + "%";
+        sliderElement.style.width = "100%";
+      }
+    });
   }
 
   const openProductModal = (productId) => {
@@ -239,48 +298,6 @@
   prevItemButtons.forEach(b => b.addEventListener("click", () => {
     if (modalSplide) modalSplide.go("<");
   }))
-
-  /************
-   TABS
-   ************/
-
-  /** @param elem {Element} */
-  const swapTabClassNamesOnSelected = (selectedTabId, selectedClassName) => (tabElement) => {
-    const tabButtonId = tabElement.getAttribute("data-tab")
-    if (tabButtonId == selectedTabId) {
-      if (!tabElement.classList.contains(selectedClassName)) {
-        tabElement.classList.add(selectedClassName)
-      }
-    } else if (tabElement.classList.contains(selectedClassName))  {
-      tabElement.classList.remove(selectedClassName)
-    }
-  }
-
-  /**
-   * @param tabCollections {NodeListOf<Element>}
-   * @param tabButtons {NodeListOf<Element>}
-   * @returns {(function(Event): void)|*}
-   */
-  const onTabButtonClick = (tabButtons, tabCollections) => (clickEvent) => {
-    const tabElem = clickEvent.target
-    const tabId = tabElem.getAttribute("data-tab")
-    if (!tabId || tabId == "") return
-
-    tabButtons.forEach(swapTabClassNamesOnSelected(tabId, "tab-collection__tab--active"))
-    tabCollections.forEach(swapTabClassNamesOnSelected(tabId, "tab-collection__content--active"))
-  }
-
-  /**
-   * @param elem {Element}
-   */
-  const initTabCollection = (elem) => {
-    const tabButtons = elem.querySelectorAll(".tab-collection__tab")
-    const tabCollections = elem.querySelectorAll(".tab-collection__content")
-
-    tabButtons.forEach(tabButton => {
-      tabButton.addEventListener("click", onTabButtonClick(tabButtons, tabCollections) )
-    })
-  }
 
   document.querySelectorAll(".tab-collection").forEach(initTabCollection)
 })();
